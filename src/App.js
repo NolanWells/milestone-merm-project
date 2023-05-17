bbde2ef48e8d5ebd262c3b61c6843f2a10c7ac2f
Bailey Franklin <baileydfranklin@gmail.com>
2023-05-16 20:44:11 -0700

    Pusing to create branches

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import './App.css';
-
+// import './App.css';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Nav from "react-bootstrap/Nav"
+import Container from "react-bootstrap/Container";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Test
-      </header>
-    </div>
+    <>
+      <Router>
+        <Container>
+          <Nav>
+
+            {/* home */}
+            <Nav.Item>
+              <Nav.Link href="/">
+                <Link to='/'>Home</Link>
+              </Nav.Link>
+            </Nav.Item>
+
+            
+            <Nav.Item>
+              <Nav.Link href="/">
+                <Link to='/'>Home</Link>
+              </Nav.Link>
+            </Nav.Item>
+
+
+
+
+
+
+
+
+          </Nav>
+        </Container>
+      </Router>
+    </>
   );
 }
 
