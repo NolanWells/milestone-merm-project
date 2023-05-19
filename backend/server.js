92cd3a114d1f414a0a682ccb673b8f6d01bf8b63
Jose <josemanuel21302@gmail.com>
2023-05-18 19:56:05 -0700

    backend folder now serves the react app

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const path = require('path')
 
+const movies = require('./controller/movies_controller')
+
+
+
+
+//middleware   
+// Have Node serve the files for our built React app
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
-    res.render()
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 })
 
+app.use('/movies', movies)
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`);
