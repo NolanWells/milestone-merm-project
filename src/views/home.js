2c1555d4c3823fc98982796ca0b998014feff7b0
Jose <josemanuel21302@gmail.com>
2023-05-21 13:34:55 -0700

    made images visable

--- src/views/home.js
+++ src/views/home.js
@@ -5,39 +5,53 @@ import { useState, useEffect } from "react";
 export default function Home() {
   const [movies, setMovies] = useState(null);
 
-  const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+  const url =
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODAwODQ0Y2E3NTAwNTVjNzIwZjdlNDk3MjUzNWYwYiIsInN1YiI6IjY0NjYzZDBkMDA2YjAxMDEwNTg4ZmJiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hBRpnvHiUVIBXbwhW9islWlaKvT-EaccFSPfZtwrvRE'
-    }
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODAwODQ0Y2E3NTAwNTVjNzIwZjdlNDk3MjUzNWYwYiIsInN1YiI6IjY0NjYzZDBkMDA2YjAxMDEwNTg4ZmJiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hBRpnvHiUVIBXbwhW9islWlaKvT-EaccFSPfZtwrvRE",
+    },
   };
-  
-
-
 
   useEffect(() => {
     fetch(url, options)
-    .then(res => res.json())
-    .then(json => setMovies(json))
+      .then((res) => res.json())
+      .then((json) => setMovies(json));
     // .then(json => console.log(json))
-}, []);
-// .catch(err => console.error('error:' + err))
+  }, []);
+  // .catch(err => console.error('error:' + err))
+
+  const getImageUrl = (posterPath) => {
+    const baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+    return baseImageUrl + posterPath;
+  };
+
+
 
   return (
     <Container>
-        {console.log(movies)}
-        {movies ? (
-            <Card>
-            {movies.results.map((movie) => (
-
-                <p key={movie.id}>{movie.original_title}</p>
-            ))}
+      {console.log(movies)}
+      {movies ? (
+        <Card>
+          {movies.results.map((movie) => (
+            <Card key={movie.id}>
+              <Card.Img
+                variant="top"
+                src={getImageUrl(movie.poster_path)}
+                alt="Movie poster"
+              />
+              <Card.Body>
+                <Card.Title>{movie.original_title}</Card.Title>
+              </Card.Body>
             </Card>
-        ) : (
-          "loading.."
-        )}
+          ))}
+        </Card>
+      ) : (
+        "loading.."
+      )}
     </Container>
   );
 }
