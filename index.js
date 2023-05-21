2ddb327ac97c9d20aa5b4d354f1317d7286d3924
Jose <josemanuel21302@gmail.com>
2023-05-21 13:11:18 -0700

    fisnihed fetch call

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const methodOverride = require('method-override')
 // Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+// app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 
 // Controllers & Routes
-app.use('/pages', require('./controller/pages'))
+// app.use('/pages', require('./controller/pages'))
 
 app.get('/', (req, res) => {
     res.render('home')
--- src/views/home.js
+++ src/views/home.js
@@ -1,24 +1,43 @@
-import Container from "react-bootstrap/esm/Container"
-import Card from 'react-bootstrap/Card';
+import Container from "react-bootstrap/esm/Container";
+import Card from "react-bootstrap/Card";
 import { useState, useEffect } from "react";
 
-export default function Home () {
-        const [movies, setMovies] = useState(null)      
-        useEffect(() => {         
-            
-            fetch('https://api.themoviedb.org/3/discover/movie?api_key=<<API Key>>')         
-            .then(data => data.json())       
-            .then(jsonData => setMovies(jsonData))
+export default function Home() {
+  const [movies, setMovies] = useState(null);
 
-        }, [])
-    
+  const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODAwODQ0Y2E3NTAwNTVjNzIwZjdlNDk3MjUzNWYwYiIsInN1YiI6IjY0NjYzZDBkMDA2YjAxMDEwNTg4ZmJiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hBRpnvHiUVIBXbwhW9islWlaKvT-EaccFSPfZtwrvRE'
+    }
+  };
+  
 
-    return(
-        <Container>
+
+
+  useEffect(() => {
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => setMovies(json))
+    // .then(json => console.log(json))
+}, []);
+// .catch(err => console.error('error:' + err))
+
+  return (
+    <Container>
+        {console.log(movies)}
+        {movies ? (
             <Card>
-                {movies ? <pre>{JSON.stringify(movies, null, 2)}</pre> : 'loading..'}
-            </Card>
-        </Container>
-    )
+            {movies.results.map((movie) => (
 
+                <p key={movie.id}>{movie.original_title}</p>
+            ))}
+            </Card>
+        ) : (
+          "loading.."
+        )}
+    </Container>
+  );
 }
