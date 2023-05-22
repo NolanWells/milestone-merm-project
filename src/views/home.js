1f600ccef2faa80d158b0f397a5189b120963c4d
Amigostres <josemanuel21302@gmail.com>
2023-05-22 02:17:36 -0700

    added some temp inline-styling using bootstrap(s) flexbox

--- src/views/home.js
+++ src/views/home.js
@@ -6,7 +6,7 @@ export default function Home() {
   const [movies, setMovies] = useState(null);
 
   const url =
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_original_language=en";
   const options = {
     method: "GET",
     headers: {
@@ -17,6 +17,7 @@ export default function Home() {
   };
 
   useEffect(() => {
+    //this request data from themovieAPI
     fetch(url, options)
       .then((res) => res.json())
       .then((json) => setMovies(json));
@@ -24,6 +25,7 @@ export default function Home() {
   }, []);
   // .catch(err => console.error('error:' + err))
 
+  //this will take the "poster_path" property and combine to base path 
   const getImageUrl = (posterPath) => {
     const baseImageUrl = 'https://image.tmdb.org/t/p/w500';
     return baseImageUrl + posterPath;
@@ -33,22 +35,33 @@ export default function Home() {
 
   return (
    <Container>
-      {movies ? (
-        movies.results.map((movie) => (
-          <Card key={movie.id}>
-            <Card.Img
-              variant="top"
-              src={getImageUrl(movie.poster_path)}
-              alt="Movie poster"
-            />
-            <Card.Body>
-              <Card.Title>{movie.original_title}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))
-      ) : (
-        "loading.."
-      )}
+      <div className="d-flex flex-wrap">
+        {movies ? (
+          //this will make cards for every movie
+          movies.results.map((movie) => (
+            <div className="col-lg-3 col-md-4 col-sm-6" key={movie.id}>
+            <Card 
+              className="d-flex flex-column justify-content-center align-items-center" 
+              key={movie.id}
+              style={{ width: "200px", height: "300px" }}
+              >
+              <Card.Img
+                variant="top"
+                src={getImageUrl(movie.poster_path)}
+                alt="Movie poster"
+                style={{ width: "150px", height: "auto", objectFit: "cover" }}
+                className="rounded-circle"
+              />
+              <Card.Body>
+                <Card.Title>{movie.original_title}</Card.Title>
+              </Card.Body>
+            </Card>
+            </div>
+          ))
+        ) : (
+          "loading.."
+        )}
+      </div>
     </Container>
   );
 }
