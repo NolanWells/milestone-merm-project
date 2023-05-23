c370dfb116fd40c762d722e895a56932443423f6
Bailey Franklin <baileydfranklin@gmail.com>
2023-05-23 15:25:10 -0700

    Added CardGroups, imported more info from API

--- src/views/details.js
+++ src/views/details.js
@@ -1,7 +1,9 @@
 import Container from "react-bootstrap/esm/Container";
 import Card from "react-bootstrap/Card";
+import { Row, Col } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { CardGroup } from "react-bootstrap/esm";
 
 export default function Home() {
   const [details, setDetails] = useState(null);
@@ -10,16 +12,16 @@ export default function Home() {
   const location = useLocation()
   const params = new URLSearchParams(location.search)
   const movie_id = params.get('data')
-  
+
   const url =
     `https://api.themoviedb.org/3/movie/${movie_id}?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_original_language=en`;
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODAwODQ0Y2E3NTAwNTVjNzIwZjdlNDk3MjUzNWYwYiIsInN1YiI6IjY0NjYzZDBkMDA2YjAxMDEwNTg4ZmJiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hBRpnvHiUVIBXbwhW9islWlaKvT-EaccFSPfZtwrvRE'
-      }
-    };
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODAwODQ0Y2E3NTAwNTVjNzIwZjdlNDk3MjUzNWYwYiIsInN1YiI6IjY0NjYzZDBkMDA2YjAxMDEwNTg4ZmJiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hBRpnvHiUVIBXbwhW9islWlaKvT-EaccFSPfZtwrvRE'
+    }
+  };
 
   // REQUESTS DATA FROM THE API
   useEffect(() => {
@@ -34,8 +36,35 @@ export default function Home() {
     return baseImageUrl + posterPath;
   };
 
-return(
-    <Card>{details && details.original_title} Test</Card>
-);
+  return (
+    <Container>
+      <CardGroup>
+        {/* // CARD 1 */}
+        <Card
+          className="d-flex flex-column justify-content-center align-items-center"
+          key={details && details.id}
+        >
+          <h5>{details && details.original_title}</h5>
+          <Card.Img
+            variant="top"
+            src={getImageUrl(details && details.poster_path)}
+            alt="Movie poster"
+            style={{ width: "200px", height: "300", objectFit: "cover" }}
+          />
+          <Card.Body><details><summary><b><u>Overview</u></b></summary>{details && details.overview}</details></Card.Body>
+          <Card.Body><details><summary><b><u>Other Information</u></b></summary><b>Release Date:</b> {details && details.release_date}<br /><b>Runtime:</b> {details && details.runtime} minutes</details></Card.Body>
+        </Card>
 
+        {/* // CARD 2 */}
+        <Card>
+          <Card.Body>Reviews</Card.Body>
+        </Card>
+
+        {/* // CARD 3 */}
+        <Card>
+          <Card.Body>Add Your Review</Card.Body>
+        </Card>
+      </CardGroup>
+    </Container>
+  );
 }
