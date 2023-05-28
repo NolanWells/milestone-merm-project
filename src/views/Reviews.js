a8dcc0fa9314757be42131cf18183439efb2ddc3
Bailey Franklin <baileydfranklin@gmail.com>
2023-05-28 12:33:24 -0700

    Made a few more CSS changes

--- src/views/Reviews.js
+++ src/views/Reviews.js
@@ -37,7 +37,7 @@ export default function Reviews({ movie_id }) {
         {reviews.map((review, index) => (
 
           <Card.Body key={index}>
-            <div>
+            <div style={{ borderBottom: 'solid gray 1px'}}>
               <Card.Text>Comment: {review.comment}</Card.Text>
               <Card.Text>Rating: { starRating(review.rating) }</Card.Text>
             </div>
