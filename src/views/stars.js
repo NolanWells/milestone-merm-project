851454e81aff3f78434e2a13fb39717096f4d8d9
Bailey Franklin <baileydfranklin@gmail.com>
2023-05-28 08:39:33 -0700

    Added code to 'stars.js' File

--- src/views/stars.js
+++ src/views/stars.js
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
 import React from "react"
-import Container from "react-bootstrap/esm/Container";
-import Card from "react-bootstrap/Card";
+import { Container, Card } from "react-bootstrap";
+// here I will use axios to request data from the server => databse
+import axios from "axios";
 
-export default function Stars() {
+export default function Stars({ movie_id }) {
+    const [starReviews, setStarReviews] = useState([])
+
+    useEffect(() => {
+        axios.get(`/movies/id/${movie_id}`)
+            .then((response) => {
+                setStarReviews(response.data)
+            })
+            .catch((err) => console.log(`axios request didnt work :( ${err}`))
+    }, [movie_id])
 
     let star = ''
     let rating = 5
@@ -11,12 +22,12 @@ export default function Stars() {
         star += '⭐'
     }
 
-    if (rating == 0){
+    if (rating == 0) {
         star = 'No reviews at this time'
     }
 
     return (
-        
+
         <Container className="for-control">
             <div style={{ display: "flex", justifyContent: "center", textAlign: "center" }}>{star}</div>
         </Container>
