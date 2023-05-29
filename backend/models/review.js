a09b525fc8c51b40004e5e7a3938f5a32089c914
Stanley Jackson <stanleyjackson11.sj@gmail.com>
2023-05-29 09:31:20 -0700

    testing

--- backend/models/review.js
+++ backend/models/review.js
@@ -6,6 +6,7 @@ const reviewSchema = new mongoose.Schema({
     comment: {type: String, required: true},
     rating: {type: Number, required: true},
     movie_id: {type: Number, required: true}
+
     //we need to find a way to get the movie_id query in the forum so we an send it.true},
 })
 
