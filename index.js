4094771d1afd1a9cc2685734a274261a48d8566d
Bailey Franklin <baileydfranklin@gmail.com>
2023-05-29 20:58:35 -0700

    testing heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Modules and Globals
 require('dotenv').config()
+
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
@@ -23,6 +25,18 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.render('error404')
 })
+
+// HEROKU TEST - IGNORE / DELETE IF YOU WANT TO
+.get('/times', (req, res) => res.send(showTimes()))
+
+function showTimes() {
+    const times = process.env.TIMES || 5
+    let result = ''
+    for (i =0; i < times; i++) {
+        result += I + ' '
+    }
+    return result
+}
 
 // Listen for Connections
 app.listen(process.env.PORT)
