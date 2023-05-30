e2eb62bfa88b4adece64f3e5fa5be9a730f10119
Bailey Franklin <baileydfranklin@gmail.com>
2023-05-30 16:37:17 -0700

    ignore

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,10 @@ const movies = require('./controller/movies_controller')
 app.use(express.json());
 app.use(express.static("public"));
 
-
+app.use('/movies', movies)
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.join(__dirname, '../build')));
-
-app.use('/movies', movies)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
